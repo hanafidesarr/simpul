75b1098a8f665952ae9c8b865461c745fc6beae1
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-06-26 11:23:27 +0700

    Redesign

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -3,6 +3,6 @@ class RoomMessage < ApplicationRecord
   belongs_to :room
 
   def as_json(options)
-    super(options).merge(user_avatar_url: user.url_image).merge(user_id: user.id)
+    super(options).merge(user_avatar_url: user.url_image).merge(user_id: user.id).merge(update_time: self.updated_at.strftime("%d/%m/%Y at %I:%M"))
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module Simpul
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Asia/Jakarta'
+    config.active_record.default_timezone = :local
   end
 end
